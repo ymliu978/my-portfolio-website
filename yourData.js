b65496797ddd9bc5a40056c128c72535c13d1fd5
Yamiao Liu <ymliu97@gmail.com>
2023-03-31 12:50:10 -0400

    major update

--- yourData.js
+++ yourData.js
@@ -6,46 +6,46 @@ export default {
   name: "Yamiao",
   headerTaglineOne: "Hello 👋",
   headerTaglineTwo:
-    "I'm Yamiao Liu - a web developer and designer based in Ottawa, Canada.",
+    "I'm Yamiao Liu",
 
   socials: [
     { title: "Resume", link: "https://drive.google.com/file/d/1zLgexcaHi6KTGygd0hJnvHSmpyJT2K-C/view?usp=sharing" },
     { title: "Github", link: "https://github.com/ymliu978" },
     { title: "LinkedIn", link: "https://www.linkedin.com/in/yamiaoliu/" },
     { title: "Instagram", link: "https://www.instagram.com/ymliu978/" },
-    { title: "Email", link: "mailto:liu00561@algonquinlive.com" },
+    { title: "Email", link: "mailto:ymliu97@gmail.com" },
   ],
 
   // End Header Details -----------------------
 
   // Work Section ------------------------
   projects: [
-    {
-      title: "Peter Logan's Steak House", //Project Title - Add Your Project Title Here
-      description: "CSS Flexbox and Grid, JavaScript, Adobe XD, Netlify",
-      //Project Image - Add Your Project Image Here
-      // imageSrc:"https://i.ibb.co/56BdKyd/peter-logan-landing-4x6.jpg",
-      imageSrc:"images/peter-logan-landing-4x6.jpg",
-      //Project URL - Add Your Project Url Here
-      url: "https://peter-logans-steakhouse.netlify.app/",
-    },
-    {
-      title: "React Travel Journal", //Project Title - Add Your Project Title Here
-      description: "ReactJS, React Props, React Compinents, Figma, Vercel",
-      //Project Image - Add Your Project Image Here
-      imageSrc:
-        "images/desktop-view.png",
-      //Project URL - Add Your Project Url Here
-      url: "https://travel-journal-react-zeta.vercel.app/",
-    },
-    {
-      title: "The Invisible Guardian", //Project Title - Add Your Project Title Here
-      description: "As a game designer, Tencent",
-      //Project Image - Add Your Project Image Here
-      imageSrc:"images/Invisible-Guardian-min.png",
-      //Project URL - Add Your Project Url Here
-      url: "https://store.steampowered.com/app/998940/_The_Invisible_Guardian/?cc=cn",
-    },
+    // {
+    //   title: "Peter Logan's Steak House", //Project Title - Add Your Project Title Here
+    //   description: "CSS Flexbox and Grid, JavaScript, Adobe XD, Netlify",
+    //   //Project Image - Add Your Project Image Here
+    //   // imageSrc:"https://i.ibb.co/56BdKyd/peter-logan-landing-4x6.jpg",
+    //   imageSrc:"images/peter-logan-landing-4x6.jpg",
+    //   //Project URL - Add Your Project Url Here
+    //   url: "https://peter-logans-steakhouse.netlify.app/",
+    // },
+    // {
+    //   title: "React Travel Journal", //Project Title - Add Your Project Title Here
+    //   description: "ReactJS, React Props, React Compinents, Figma, Vercel",
+    //   //Project Image - Add Your Project Image Here
+    //   imageSrc:
+    //     "images/desktop-view.png",
+    //   //Project URL - Add Your Project Url Here
+    //   url: "https://travel-journal-react-zeta.vercel.app/",
+    // },
+    // {
+    //   title: "The Invisible Guardian", //Project Title - Add Your Project Title Here
+    //   description: "As a game designer, Tencent",
+    //   //Project Image - Add Your Project Image Here
+    //   imageSrc:"images/Invisible-Guardian-min.png",
+    //   //Project URL - Add Your Project Url Here
+    //   url: "https://store.steampowered.com/app/998940/_The_Invisible_Guardian/?cc=cn",
+    // },
     /*
     If You Want To Add More Project just Copy and Paste This At The End
     ,{
@@ -61,6 +61,32 @@ export default {
 
  // Game Section ------------------------
  projects2: [
+  {
+    title: "uOttaHack 5 Winner", //Project Title - Add Your Project Title Here
+    description: "React, OCR, APIs, Sponsor Chanllenge 🥉",
+    //Project Image - Add Your Project Image Here
+    // imageSrc:"https://i.ibb.co/56BdKyd/peter-logan-landing-4x6.jpg",
+    imageSrc:"images/uottahack5.png",
+    //Project URL - Add Your Project Url Here
+    url: "https://devpost.com/software/care-scan",
+  },
+  // {
+  //   title: "React Travel Journal", //Project Title - Add Your Project Title Here
+  //   description: "ReactJS, React Props, React Compinents, Figma, Vercel",
+  //   //Project Image - Add Your Project Image Here
+  //   imageSrc:
+  //     "images/desktop-view.png",
+  //   //Project URL - Add Your Project Url Here
+  //   url: "https://travel-journal-react-zeta.vercel.app/",
+  // },
+  {
+    title: "The Invisible Guardian", //Project Title - Add Your Project Title Here
+    description: "As a game designer, Tencent",
+    //Project Image - Add Your Project Image Here
+    imageSrc:"images/Invisible-Guardian-169.jpg",
+    //Project URL - Add Your Project Url Here
+    url: "https://store.steampowered.com/app/998940/_The_Invisible_Guardian/?cc=cn",
+  },
   {
     title: "The Master", //Project Title - Add Your Project Title Here
     description: "Unity, Physics Game",
@@ -102,6 +128,15 @@ export default {
   //   url: "https://iloveyoubut.cargo.site/About",
   // },
   {
+    title: "Peter Logan's Steak House", //Project Title - Add Your Project Title Here
+    description: "Web Dev, Dark Mode, Multi Languages, Adobe XD",
+    //Project Image - Add Your Project Image Here
+    // imageSrc:"https://i.ibb.co/56BdKyd/peter-logan-landing-4x6.jpg",
+    imageSrc:"images/peter-logan-landing-4x6.jpg",
+    //Project URL - Add Your Project Url Here
+    url: "https://peter-logans-steakhouse.netlify.app/",
+  },
+  {
     title: "A Donut", //Project Title - Add Your Project Title Here
     description: "Blender, 3D Modelling",
     //Project Image - Add Your Project Image Here
@@ -117,10 +152,15 @@ export default {
   // Services Section ---------------
 
   services: [
+    // {
+    //   title: "Web Design and Development",
+    //   description:
+    //     "I have a keen eye for aesthetics and the ability to design websites from scratch, then translate the visual design into adaptable code.",
+    // },
     {
-      title: "Web Design and Development",
+      title: "Mutlimedia Design and Development",
       description:
-        "I have a keen eye for aesthetics and the ability to design websites from scratch, then translate the visual design into adaptable code.",
+        "I have a keen eye for aesthetics and the ability to design and develop games, websites, and other digital products.",
     },
     {
       title: "Communication and Problem-solving",
@@ -128,14 +168,14 @@ export default {
         "I know how to effectively communicate with clients via email and phone to bring their vision to life. I am self-directed and adopt new processes quickly.",
     },
     {
-      title: "Technologies",
-      description:
-        "HTML5, CSS3, Bootstrap, JavaScript, ReactJS, jQuery, C#, ASP.Net, SQL, Python, PHP, and more...",
-    },
-    {
       title: "Tools",
       description:
-        "Visual Studio, Git, MySQL Workbench, Unity Engine, Blender, Adobe InDesign, Adobe XD, Adobe Premiere Pro, and more...",
+        "Unity Engine, Blender, Visual Studio, Git, MySQL Workbench, Adobe InDesign, Adobe XD, Adobe Premiere Pro, and more...",
+    },
+    {
+      title: "Technologies",
+      description:
+        "HTML5, CSS3, JavaScript, ReactJS, jQuery, C#, SQL, Python, PHP, and more...",
     },
   ],
 
@@ -143,7 +183,7 @@ export default {
 
   // About Secton --------------
   aboutparaone:
-    "I am a web creator that specializes in designing and developing interactive digital experiences. I love bridging the gap between art and programming.",
+    "I am a creator that specializes in designing and developing interactive digital experiences. I love bridging the gap between art and programming.",
   aboutparatwo:
     "Seriously, they make each other better.",
   //   End About Section ---------------------
